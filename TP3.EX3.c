2991c49e098be5ebc936f665f16b5eadbd87fbea
essia ghoul <32401106+Essiagh@users.noreply.github.com>
2017-10-11 13:11:24 +0100

    Add files via upload

---
+++ TP3.EX3.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : EX3.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a,i,e;
+	do {
+		printf("taper un entier positif");
+		scanf("%d",&e);
+	}while(e<0);
+a=1;
+for(j=2;j<=x;j++)
+{
+	if(e%j=0);
+	a=a+1;
+}
+if (a==2)
+	printf("%d est premier",e);
+else printf("%d est non premier",e);
+}
+
