82c723bcb97c9736af31f282b7af3ff29fb0f1db
essia ghoul <32401106+Essiagh@users.noreply.github.com>
2017-10-11 13:15:04 +0100

    Add files via upload

---
+++ Tp3.ex2.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a,b,r;
+	do {
+		printf("taper un entier a");
+	    scanf("%d",&a);
+	    printf("taper un entier b");
+	    scanf("%d",&b);
+	}while(a<b);
+	r=a%b;
+	while()
+	{ a=b;
+	  b=r;
+
+	}
+
